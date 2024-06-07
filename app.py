1b26cd6cb2c5eff76f2843b903bb85f69b64c71c
Flavio Ruvalcaba Leija <71139354+FlavioRr@users.noreply.github.com>
2024-06-06 18:23:21 -0600

    update app

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def upload():
         return 'Error: AST extraction failed for one or both files.', 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
